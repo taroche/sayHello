f4b52fafbfce4b6e6d6f9bdd3655387f22dd5043
Tristan LaRoche <laroche11235@gmail.com>
2018-11-08 20:11:30 -0500

    add else statement to say something

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
-(function () {
+
 const input = document.querySelector('input');
 
 input.addEventListener('keypress', addName);
 
 function addName(event) {
     if (event.which === 13) {
-        const p = document.createElement('p');
-       p.innerText = `Hello ${input.value}, nice to meet you!`;
-        const form = document.querySelector('form');
-        form.append(p);
-        event.preventDefault();   
+        if (input.value.length === 0) {
+            const p = document.createElement('p');
+            p.innerText = `The text field is empty. Please enter name.`;
+             const form = document.querySelector('form');
+             form.append(p);
+             event.preventDefault();
+        }
+        else {
+            const p = document.createElement('p');
+            p.innerText = `Hello ${input.value}, nice to meet you!`;
+            const form = document.querySelector('form');
+            form.append(p);
+            event.preventDefault();
+        }   
    }
 }
-
-})();
