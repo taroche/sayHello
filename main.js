85215b0a425e6aaab295e9080f32f353ca808bfc
Tristan LaRoche <laroche11235@gmail.com>
2018-11-09 15:44:42 -0500

    can clear input after enter

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const input = document.querySelector('input');
 
 input.addEventListener('keypress', addName);
 
+
 function addName(event) {
     if (event.which === 13) {
         if (input.value.length === 0) {
@@ -10,14 +11,14 @@ function addName(event) {
             p.innerText = `The text field is empty. Please enter name.`;
              const form = document.querySelector('form');
              form.append(p);
-             event.preventDefault();
         }
         else {
             const p = document.createElement('p');
             p.innerText = `Hello ${input.value}, nice to meet you!`;
             const form = document.querySelector('form');
             form.append(p);
-            event.preventDefault();
-        }   
+            form.reset(); 
+        } 
+        event.preventDefault(); 
    }
 }
