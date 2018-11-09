2d49c979744d944bbd810a94ba855e0544d9554a
Tristan LaRoche <laroche11235@gmail.com>
2018-11-08 19:39:33 -0500

    can add <p> with text

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-const input = document.querySelector(input)
+(function () {
+const input = document.querySelector('input');
 
-input.addEventListener('keypress', addName)
+input.addEventListener('keypress', addName);
+
+function addName(event) {
+    if (event.which === 13) {
+        const p = document.createElement('p');
+       p.innerText = `Hello ${input.value}, nice to meet you!`;
+        const form = document.querySelector('form');
+        form.append(p);
+        event.preventDefault();   
+   }
+}
+
+})();
